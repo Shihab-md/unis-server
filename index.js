b72fb93d7a23940203cc35ae0619af414f824f32
Shihab-md <nmd.shihabudeen@gmail.com>
2025-02-23 21:20:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import authRouter from './routes/auth.js'
 import departmentRouter from './routes/department.js'
+import schoolRouter from './routes/school.js'
 import employeeRouter from './routes/employee.js'
 import salaryRouter from './routes/salary.js'
 import leaveRouter from './routes/leave.js' 
@@ -19,6 +20,7 @@ app.use(express.json())
 app.use(express.static('public/uploads'))
 app.use('/api/auth', authRouter)
 app.use('/api/department', departmentRouter)
+app.use('/api/school', schoolRouter)
 app.use('/api/employee', employeeRouter)
 app.use('/api/salary', salaryRouter)
 app.use('/api/leave', leaveRouter)
