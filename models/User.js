a13c82fcb1a54ef00a04048431ff903be0ae215e
Shihab-md <nmd.shihabudeen@gmail.com>
2025-03-02 18:46:44 +0900

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     email: {type: String, required: true},
     password: {type: String, required: true},
     role: {type: String, enum: ["admin", "employee", "supervisor", "teacher", "student", "parent"], required: true},
-    profileImage: {type: String},
+    profileImage: {type: Blob},
     createAt: {type: Date, default: Date.now},
     updatedAt: {type: Date, default: Date.now},
 })
