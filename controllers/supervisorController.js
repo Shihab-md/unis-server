1dbb2bbbbf009e586303104b8e26ec899ca57015
Shihab-md <nmd.shihabudeen@gmail.com>
2025-03-08 04:57:50 +0900

    Update supervisorController.js

--- controllers/supervisorController.js
+++ controllers/supervisorController.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage }).single('image');
 
 const addSupervisor = async (req, res) => {
   try {
