6608d3e2a0f38d40d74b7d5a0859ecf1c641ad55
Shihab-md <nmd.shihabudeen@gmail.com>
2025-03-08 21:06:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express()
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://unis-frontend.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.unis.org.in/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
