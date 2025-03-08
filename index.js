9aa39656060a8dcc913322ccc40f3665b628da41
Shihab-md <nmd.shihabudeen@gmail.com>
2025-03-09 01:31:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import connectToDatabase from './db/db.js'
 
 connectToDatabase() 
 const app = express() 
-app.use(cors()) 
+//app.use(cors()) 
 
 app.use(express.json())
 app.use(express.static('public/uploads'))
