71f1d7a7f8d09be6d0be790acd5df8e2fac6cf72
Shihab-md <nmd.shihabudeen@gmail.com>
2025-03-08 22:09:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -18,24 +18,24 @@ const app = express()
 //app.use(cors({origin: 'https://www.unis.org.in'}))
 //const cors = require('cors');
 // Add headers
-app.use(function (req, res, next) {
+//app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+ //   res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'content-type,Authorization');
+ //   res.setHeader('Access-Control-Allow-Headers', 'content-type,Authorization');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+ //   res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
-    next();
-});
+ //   next();
+//});
 
 //app.use(cors())
 
