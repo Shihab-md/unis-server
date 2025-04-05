7c64f9f65c3698782d010f935ef93b5f4fda7032
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-06 00:32:38 +0900

    Update supervisorController.js

--- controllers/supervisorController.js
+++ controllers/supervisorController.js
@@ -21,7 +21,7 @@ const storage = multer.diskStorage({
 });
 
 //const upload = multer({ storage: storage });
-const upload = multer({ dest: 'https://www.unis.org.in/public/' })
+const upload = multer({ dest: 'https://www.unis.org.in/' })
 
 const addSupervisor = async (req, res) => {
   try {
