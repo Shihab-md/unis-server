5211b146123a191df55ca6cd72fb5a137ffdc2c9
Shihab-md <nmd.shihabudeen@gmail.com>
2025-04-13 18:37:05 +0900

    Update institute.js

--- routes/institute.js
+++ routes/institute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import authMiddleware from '../middleware/authMiddlware.js'
-import { addInstitute, upload, getInstitutes, getInstitute, updateInstitute, deleteInstitute } from '../controllers/instituteController.js'
+import { addInstitute, getInstitutes, getInstitute, updateInstitute, deleteInstitute } from '../controllers/instituteController.js'
 
 const router = express.Router()
 
@@ -9,6 +9,5 @@ router.post('/add', authMiddleware, upload.single('image'), addInstitute)
 router.get('/:id', authMiddleware, getInstitute)
 router.put('/:id', authMiddleware, updateInstitute)
 router.delete('/:id', authMiddleware, deleteInstitute)
-//router.get('/department/:id', authMiddleware, fetchInstitutesByDepId)
 
 export default router
