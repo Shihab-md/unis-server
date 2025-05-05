d86f39a3437733b97903bf87fc5ddcdbe3b6f068
Shihab-md <nmd.shihabudeen@gmail.com>
2025-05-05 15:11:13 +0900

    Update supervisorController.js

--- controllers/supervisorController.js
+++ controllers/supervisorController.js
@@ -6,7 +6,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: async function (req, file, cb) {
-    cb(null, "./public/uploads/");
+    cb(null, "./uploads/");
   },
 
   filename: (req, file, cb) => {
