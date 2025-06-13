fbbf5e658bc56378b627d788760d130feec52690
Shihab-md <nmd.shihabudeen@gmail.com>
2025-06-14 01:09:34 +0900

    Update redis.js

--- db/redis.js
+++ db/redis.js
@@ -2,7 +2,7 @@ import { createClient } from 'redis';
 
 const redisClient = createClient({
     url: process.env.REDIS_URL,
-    connectTimeout: 10000 // 10 seconds
+    connectTimeout: 25000 // 25 seconds
 });
 
 redisClient.on('error', err => console.log('Redis Client Error', err));
